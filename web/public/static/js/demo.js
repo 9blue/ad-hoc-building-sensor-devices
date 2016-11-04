ea4c195982579aaa452f7dc9c1805bbe70d1fa1a
Zhennan Wang <zhennan.wang@me.com>
2016-11-04 00:17:13 -0700

    get rid of hardcode hashcode

--- web/public/static/js/demo.js
+++ web/public/static/js/demo.js
@@ -20,25 +20,26 @@
 
         appRef.once('value').then(function(snapshot) {
             vm.app_config = snapshot.val();
-            console.log(snapshot.val());
             instances = _.keys(vm.app_config.instances);
-            console.log(instances);
         });
 
         appRef.child('instances').on('child_added', function(data) {
             var new_instance = data.key;
             var instanceRef = dataRef.child(new_instance);
-
+            var actuator = null;
+            instanceRef.child('flash').once("value").then(function(snapshot) {
+                actuator = _.keys(snapshot.val())[0];
+            });
             instanceRef.child('light').on('value', function(snapshot) {
                 $timeout(function() {
                     var device_id = _.keys(snapshot.val())[0];
                     vm.light_reading = snapshot.val()[device_id];
                     if (vm.light_reading < 100) {
                         vm.flash_on = true;
-                        instanceRef.child('flash').limitToFirst(1).set(true);
+                        instanceRef.child('flash').child(actuator).set(true);
                     } else {
                         vm.flash_on = false;
-                        instanceRef.child('flash').limitToFirst(1).set(false);
+                        instanceRef.child('flash').child(actuator).set(false);
                     }
                 }, 0);
             });
