9874350573256e31d2f63964fa6a5b6292b57c58
Zhennan Wang <zhennan.wang@me.com>
2016-11-05 23:08:23 -0700

    refactor logic builder

--- web/public/static/js/logicBuilder.js
+++ web/public/static/js/logicBuilder.js
@@ -1,46 +1,39 @@
 (function() {
     'use strict';
-
     madAPP.controller('logicBuilderController', logicBuilderController);
 
-    function logicBuilderController($stateParams) {
+    function logicBuilderController(sensorService, $window) {
         var lb = this;
+        var dbRef = firebase.database().ref();
+        var appRef = dbRef.child('apps');
 
         lb.added_sensors = [];
 
-        lb.sensor_list = [
-            { name: 'light', label: 'Light Sensor', type: 'dark' },
-            { name: 'sound', label: 'Sound Sensor', type: 'light' },
-            { name: 'camera', label: 'Camera', type: 'dark' },
-        ];
-        lb.new_sensor = lb.sensor_list[1];
+        lb.sensor_list = sensorService.getSensors();
+
+        lb.new_sensor = lb.sensor_list[0];
         lb.addSensor = function() {
-            lb.added_sensors.push('/template/sensors_tpl/' + lb.new_sensor.name + '.html');
+            lb.added_sensors.push(lb.new_sensor.tpl_url);
             console.log(lb.new_sensor);
         };
 
-        lb.acturator_list = [
-            { name: 'flash', label: 'Flash'},
-            { name: 'screen_light', label: 'Screen Light'}
-        ];
-        lb.new_acturator = lb.acturator_list[1];
+        lb.acturator_list = sensorService.getActurators();
+        lb.new_acturator = lb.acturator_list[0];
 
-        lb.addActurator = function(){
-            lb.added_acturator = '/template/acturator_tpl/' + lb.new_acturator.name + '.html';
-        }
+        lb.addActurator = function() {
+            lb.added_acturator = lb.new_acturator.tpl_url;
+        };
 
         lb.createdApp = function() {
-            var dbRef = firebase.database().ref();
-            var appRef = dbRef.child('apps');
-            console.log(lb.new_app);
             var newApp = appRef.push(
-                lb.new_app
-            , function(err) {
-                if (err) {
-                    console.log(err);
-                }
-            });
+                lb.new_app,
+                function(err) {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        $window.location.hash = "";
+                    }
+                });
         };
-        lb.modalTemplate = "template/thresholdInfo.html";
     }
 })();
