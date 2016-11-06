5ef4e3ac2f60287b32cd2ef5b9f1afd65b03e5a7
vbhutada <bhutadavrushali@gmial.com>
2016-11-05 19:03:03 -0700

    logicBuilder form submit

--- web/public/static/js/logicBuilder.js
+++ web/public/static/js/logicBuilder.js
@@ -19,38 +19,28 @@
             console.log(lb.new_sensor);
         };
 
+        lb.acturator_list = [
+            { name: 'flash', label: 'Flash'},
+            { name: 'screen_light', label: 'Screen Light'}
+        ];
+        lb.new_acturator = lb.acturator_list[1];
+
+        lb.addActurator = function(){
+            lb.added_acturator = '/template/acturator_tpl/' + lb.new_acturator.name + '.html';
+        }
+
         lb.createdApp = function() {
+            var dbRef = firebase.database().ref();
+            var appRef = dbRef.child('apps');
             console.log(lb.new_app);
+            var newApp = appRef.push(
+                lb.new_app
+            , function(err) {
+                if (err) {
+                    console.log(err);
+                }
+            });
         };
         lb.modalTemplate = "template/thresholdInfo.html";
-
-        var dbRef = firebase.database().ref();
-        var appRef = dbRef.child('apps');
-
-        var organizations = dbRef.child('organizations');
-        var apps = dbRef.child('applications');
-        lb.organizations = [];
-
-        lb.selectedOrg = null;
-
-
-        lb.id = $stateParams.id;
-        lb.text = 'This is app installer!';
-        lb.choices = [{ id: 'choice1' }];
-
-        lb.addNewChoice = function() {
-            var newItemNo = lb.choices.length + 1;
-            lb.choices.push({ 'id': 'choice' + newItemNo });
-        };
-
-        lb.removeChoice = function() {
-            if (lb.choices.length == 1) {
-                return;
-            }
-            var lastItem = lb.choices.length - 1;
-            lb.choices.splice(lastItem);
-        };
-
-
     }
 })();
