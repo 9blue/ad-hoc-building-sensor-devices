8347d571dee28f8f64d9c6441ac8cf74d50338e2
Zhennan Wang <zhennan.wang@me.com>
2016-11-08 20:27:33 -0800

    set up init view for app installer

--- web/public/static/js/appInstaller.js
+++ web/public/static/js/appInstaller.js
@@ -2,9 +2,17 @@
     'use strict';
     madAPP.controller('appInstallerController', appInstallerController);
 
-    function appInstallerController($stateParams) {
+    function appInstallerController() {
         var vm = this;
-        vm.id = $stateParams.id;
-        vm.text = 'test text!';
+        vm.apps = ['app1', 'app2', 'app3']; // Retrieve that from firebase
+
+        vm.selected_app = vm.apps[0];
+        vm.showDetail = function(app){
+            vm.selected_app = app;
+        };
+
+        vm.installApp = function(){
+            // logic to be decided here
+        };
     }
 })();
