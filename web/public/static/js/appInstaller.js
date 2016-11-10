3765653284d86ebbbd9939df69b251a5572f7c41
Zhennan Wang <zhennan.wang@me.com>
2016-11-09 17:53:31 -0800

    add qr code

--- web/public/static/js/appInstaller.js
+++ web/public/static/js/appInstaller.js
@@ -3,29 +3,33 @@
     madAPP.controller('appInstallerController', appInstallerController);
 
     function appInstallerController($timeout) {
-        var ai = this;
+        var vm = this;
         var dbRef = firebase.database().ref();
         var appsRef = dbRef.child('apps');
+        var lookUp = dbRef.child('app_ids');
 
         //vm.apps = ['app1', 'app2', 'app3']; // Retrieve that from firebase
-        ai.apps = [];
+        vm.apps = [];
 
         appsRef.once('value').then(function(snapshot) {
             $timeout(function() {
-            var apps = snapshot.val();
-            for (var k in apps) {
-                ai.apps.push(apps[k]);
-            }
-            ai.selected_app = ai.apps[0];
-            console.log(ai.apps);
+                vm.apps = snapshot.val();
+                if (!vm.selected_id) {
+                    vm.selected_id = _.keys(vm.apps)[0];
+                    vm.selected_app = vm.apps[vm.selected_id];
+                }
             }, 0);
         });
 
-        ai.showDetail = function(app){
-            ai.selected_app = app;
+
+        vm.showDetail = function(id) {
+            vm.selected_id = id;
+            vm.selected_app = vm.apps[vm.selected_id];
         };
 
-        ai.installApp = function(){
+        vm.installApp = function() {
+            vm.inst_hash = lookUp.push().key;
+            lookUp.child(vm.inst_hash).set(vm.selected_id);
             // logic to be decided here
         };
     }
