6cc4fe1d1ce337849053295ff4468469ce1f83c2
GIV <giyyanan@andrew.cmu.edu>
2016-11-16 14:31:41 -0800

    resolved merge conflicts

--- android/app/src/main/java/ini_google/ad_hoc_building_sensor_devices/mad_hoc/ui/ListActivity.java
+++ android/app/src/main/java/ini_google/ad_hoc_building_sensor_devices/mad_hoc/ui/ListActivity.java
@@ -97,96 +97,36 @@ public class ListActivity extends AppCompatActivity {
             listDataChild = new HashMap<String, List<Parameter>>();
 
             Iterator<?> keys = config.keys();
-<<<<<<< HEAD
             Parameter parameter = null;
             List<Parameter> parameters = null;
 
             while(keys.hasNext()) {
                 parameters = new ArrayList<Parameter>();
-=======
-//            Parameter parameter1 = null;
-//            Parameter parameter2 = null;
-//            Parameter parameter3 = null;
-            while(keys.hasNext()) {
-                Parameter parameter1 = null;
-                Parameter parameter2 = null;
-                Parameter parameter3 = null;
-
->>>>>>> 3ac41219bc868b77c6d382b472e446e7c03df7bb
                 String key = (String) keys.next();
                 JSONObject configSensor = (JSONObject) config.get(key);
                 parameter = null;
                 String type = configSensor.get("type").toString();
                 listDataHeader.add(key);
-<<<<<<< HEAD
-=======
-                List<Parameter> child = new ArrayList<Parameter>();
-
-//                if(lookupTable.get(type).equals("S")) {
-                if (configSensor.has("threshold")) {
-                    JSONObject params = (JSONObject) configSensor.get(key);
-                    // do we need lookuptable?
-                    if (((JSONObject)params.get("threshold")).get("fixed").toString().equals("false")) {
-                        if (((JSONObject)params.get("threshold")).has("upper")) {
-                            parameter1 = new Parameter("Threshold_upper", ((JSONObject)params.get("threshold")).get("upper").toString(), true);
-                        }
-                        if (((JSONObject)params.get("threshold")).has("lower")) {
-                            parameter2 = new Parameter("Threshold_lower", ((JSONObject)params.get("threshold")).get("lower").toString(), true);
-                        }
-                    } else {
-                        if (((JSONObject)params.get("threshold")).has("upper")) {
-                            parameter1 = new Parameter("Threshold_upper", params.get("upper").toString(), false);
-                        }
-                        if (((JSONObject)params.get("threshold")).has("lower")) {
-                            parameter2 = new Parameter("Threshold_lower", params.get("lower").toString(), false);
-                        }
-                        if (params.has("sampling_rate")) {
-                            parameter3 = new Parameter("Threshold_lower", params.get("lower").toString(), false);
-                        }
-
-                    }
->>>>>>> 3ac41219bc868b77c6d382b472e446e7c03df7bb
 
                 if (configSensor.has("threshold_upper")) {
                     parameter = null;
-                    parameter = new Parameter("Upper Threshold", configSensor.get("threshold_upper").toString(),"Int",configSensor.get("threshold_fixed").toString());
+                    parameter = new Parameter("threshold_upper", configSensor.get("threshold_upper").toString(),"Int",configSensor.get("threshold_fixed").toString());
                     parameters.add(parameter);
                 }
-<<<<<<< HEAD
                 if(configSensor.has("threshold_lower")) {
                     parameter = null;
-                    parameter = new Parameter("Lower Threshold", configSensor.get("threshold_lower").toString(),"Int",configSensor.get("threshold_fixed").toString());
+                    parameter = new Parameter("threshold_lower", configSensor.get("threshold_lower").toString(),"Int",configSensor.get("threshold_fixed").toString());
                     parameters.add(parameter);
                 }
                 if(configSensor.has("sampling_rate")) {
                     parameter = null;
-                    parameter = new Parameter("Sampling Rate", configSensor.get("sampling_rate").toString(),"Int",configSensor.get("threshold_fixed").toString());
+                    parameter = new Parameter("sampling_rate", configSensor.get("sampling_rate").toString(),"Int",configSensor.get("threshold_fixed").toString());
                     parameters.add(parameter);
                 }
                 listDataChild.put(listDataHeader.get(listDataHeader.indexOf(key)), parameters);
             }
 
 
-
-=======
-
-                //List<Parameter> child = new ArrayList<Parameter>();
-//                listDataChild.put(key, child);
-                if(parameter1 != null)child.add(parameter1);
-                if(parameter2 != null)child.add(parameter2);
-                if(parameter3 != null)child.add(parameter3);
-
-                if(child.size() > 0) {
-                    listDataChild.put(key, child);
-                }
-            }
-            //List<Parameter> child = new ArrayList<Parameter>();
-
-            //if(parameter1 != null)child.add(parameter1);
-            //if(parameter2 != null)child.add(parameter2);
-
-            //listDataChild.put(listDataHeader.get(0), child);
->>>>>>> 3ac41219bc868b77c6d382b472e446e7c03df7bb
 
         } catch (Exception e) {
             Toast.makeText(this, "Configuration Error", Toast.LENGTH_LONG).show();
@@ -214,20 +154,10 @@ public class ListActivity extends AppCompatActivity {
                for(Parameter parameter:list) {
                    String item = parameter.getItem();
                    String val = parameter.getVal();
-                   boolean editable = parameter.isEditable();
+                   boolean fixed = parameter.getFixed();
 
-                   if (item.equals("Threshold_upper")) {
-                       if (editable &&
-                               (!((JSONObject)field.get("threshold")).get("upper").equals(val))) {
+                   if(!fixed)field.put(item,Integer.parseInt(val));
 
-                           field.put("upper", val);
-                       }
-                   } else if(item.equals("Threshold_lower")) {
-                       if (editable &&
-                               (!((JSONObject)field.get("threshold")).get("lower").equals(val))) {
-                           field.put("lower", val);
-                       }
-                   }
                }
 
            }
@@ -237,24 +167,17 @@ public class ListActivity extends AppCompatActivity {
     }
 
     // define new class for parameters
-    public class Parameter{
+    public class Parameter {
         String item;
         String val;
-<<<<<<< HEAD
         String type;
         String fixed;
-        Parameter(String item, String val,String type,String fixed) {
+
+        Parameter(String item, String val, String type, String fixed) {
             this.item = item;
             this.val = val;
             this.type = type;
             this.fixed = fixed;
-=======
-        boolean editable;
-        Parameter(String item, String val, boolean editable) {
-            this.item = item;
-            this.val = val;
-            this.editable = editable;
->>>>>>> 3ac41219bc868b77c6d382b472e446e7c03df7bb
         }
 
         public String getItem() {
@@ -272,7 +195,7 @@ public class ListActivity extends AppCompatActivity {
         public void setVal(String val) {
             this.val = val;
         }
-<<<<<<< HEAD
+
         public String getType() {
             return this.type;
         }
@@ -280,26 +203,17 @@ public class ListActivity extends AppCompatActivity {
         public void setType(String Type) {
             this.type = type;
         }
+
         public Boolean getFixed() {
-            if (this.fixed.equals("true")){
+            if (this.fixed.equals("true")) {
                 return true;
-            }
-            else {
+            } else {
                 return false;
             }
         }
 
         public void setFixed(String Type) {
-            this.type = type;
-=======
-
-        public boolean isEditable() {
-            return editable;
-        }
-
-        public void setEditable(boolean editable) {
-            this.editable = editable;
->>>>>>> 3ac41219bc868b77c6d382b472e446e7c03df7bb
+            this.fixed = fixed;
         }
     }
 }
