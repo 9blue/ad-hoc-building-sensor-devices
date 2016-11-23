fd91aea88edf5e7d2cf8b97f93a9d6e1555fb9a2
Zhennan Wang <zhennan.wang@me.com>
2016-11-22 18:01:37 -0600

    part of app installer view updates

--- web/public/static/js/appInstaller.js
+++ web/public/static/js/appInstaller.js
@@ -2,33 +2,57 @@
     'use strict';
     madAPP.controller('appInstallerController', appInstallerController);
 
-    function appInstallerController($timeout) {
+    function appInstallerController($timeout, $scope) {
         var vm = this;
         var dbRef = firebase.database().ref();
         var appsRef = dbRef.child('apps');
         var lookUp = dbRef.child('app_ids');
 
-        //vm.apps = ['app1', 'app2', 'app3']; // Retrieve that from firebase
         vm.apps = [];
 
+        function getAppsPromise() {
+            return appsRef.once('value').then(function(snapshot) {
+                return snapshot;
+            });
+        }
 
+        function updateInstalls() {
+            getInstallsPromise(vm.selected_id).then(function(data) {
+                processInstalls(data);
+                $scope.$apply();
+            });
+        }
 
-        appsRef.once('value').then(function(snapshot) {
-            $timeout(function() {
-                vm.apps = snapshot.val();
-                if (!vm.selected_id) {
-                    vm.selected_id = _.keys(vm.apps)[0];
-                    vm.selected_app = vm.apps[vm.selected_id];
-                }
-            }, 0);
-        });
+        function getInstallsPromise(id) {
+            return lookUp.orderByChild("app_id").equalTo(id).once('value').then(function(snapshot) {
+                return snapshot;
+            });
+        }
 
+        function processInstalls(data) {
+            vm.all_installs = [];
+            data.forEach(function(child) {
+                var key = child.key;
+                var name = child.val().install_name;
 
-        lookUp.orderByValue().equalTo(vm.selected_id).on('child_added', function(snapshot) {
-            console.log(snapshot.key);
-            $timeout(function() {
-                vm.all_installs.append(snapshot.key);
-            }, 0);
+                vm.all_installs.push({
+                    id: key,
+                    name: name
+                });
+            });
+
+        }
+        getAppsPromise().then(function(data) {
+            vm.apps = data.val();
+            angular.forEach(vm.apps, function(app){
+                app.components = _.keys(app.default_config);
+            });
+            console.log(vm.apps);
+            if (!vm.selected_id) {
+                vm.selected_id = _.keys(vm.apps)[0];
+                vm.selected_app = vm.apps[vm.selected_id];
+            }
+            updateInstalls();
         });
 
 
@@ -36,12 +60,34 @@
         vm.showDetail = function(id) {
             vm.selected_id = id;
             vm.selected_app = vm.apps[vm.selected_id];
+            getInstallsPromise(vm.selected_id).then(function(data) {
+                processInstalls(data);
+                $scope.$apply();
+            });
         };
 
-        vm.installApp = function() {
-            vm.inst_hash = lookUp.push().key;
-            lookUp.child(vm.inst_hash).set(vm.selected_id);
-            // logic to be decided here
+        vm.openModal = function(install) {
+            if (install) {
+                vm.inst_hash = install.id;
+                vm.modal_title = "Installation " + install.name;
+            } else {
+                vm.modal_title = "Install " + vm.selected_app.app_name;
+                vm.inst_hash = lookUp.push().key;
+                lookUp.child(vm.inst_hash).set({
+                    app_id: vm.selected_id,
+                    install_name: 'New install'
+                });
+                updateInstalls();
+            }
+
+        };
+
+        vm.removeInstall = function(id) {
+            lookUp.child(id).remove();
+            dbRef.child('devices/' + id).remove();
+            dbRef.child('install_actuators／' + id).remove();
+            dbRef.child('install_sensors／' + id).remove();
+            updateInstalls();
         };
     }
 })();
