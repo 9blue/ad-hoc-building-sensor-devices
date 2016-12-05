190afc9ec727e0459bebd217078f3bafb59831cd
Marcus <mingchia.ts@gmail.com>
2016-12-05 14:33:38 -0800

    add screen page

--- android/app/src/main/java/ini_google/ad_hoc_building_sensor_devices/mad_hoc/ui/ScreenActivity.java
+++ android/app/src/main/java/ini_google/ad_hoc_building_sensor_devices/mad_hoc/ui/ScreenActivity.java
@@ -4,36 +4,33 @@ package ini_google.ad_hoc_building_sensor_devices.mad_hoc.ui;
  * Created by chilli on 11/23/16.
  */
 
-import android.content.Context;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
-import android.graphics.Rect;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 
-public class ScreenActivity extends View {
+import ini_google.ad_hoc_building_sensor_devices.R;
 
-    private Rect rectangle;
-    private Paint paint;
-
-    public ScreenActivity(Context context) {
-        super(context);
-        int x = 50;
-        int y = 50;
-        int sideLength = 200;
-
-        // create a rectangle that we'll draw later
-        rectangle = new Rect(x, y, sideLength, sideLength);
-
-        // create the Paint and set its color
-        paint = new Paint();
-        paint.setColor(Color.GREEN);
-    }
+public class ScreenActivity extends AppCompatActivity {
+    private Button cancelButton;
+    private Activity activity = this;
 
     @Override
-    protected void onDraw(Canvas canvas) {
-        canvas.drawColor(Color.BLUE);
-        canvas.drawRect(rectangle, paint);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_screen);
+        cancelButton = (Button) findViewById(R.id.cancelButton);
+
+        cancelButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                //mSensorManager.unregisterListener();
+                Intent intent = new Intent();
+                intent.setClass(activity, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 }
